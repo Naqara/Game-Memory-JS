c8363f6a7b3c5d2dfe339e48c3ad6e07682784c3
Michal Studencki <studi2005@gmail.com>
2020-02-02 19:50:08 +0100

    Finish turtorial need fixed bug

--- game.js
+++ game.js
@@ -3,15 +3,68 @@ const cardsColor = ["red", "red", "green", "green", "blue", "blue", "brown", "br
 let cards = document.querySelectorAll("div");
 cards = [...cards];
 
-const init = function() 
-{
-    cards.forEach(function(card)
-    {
-        const position = Math.floor(Math.random()*
-        cardsColor.length);
+const startTime = new Date().getTime();
+
+let activeCard = "";
+const activeCards  = [];
+
+const gameParis = cards.length / 2;
+let gameResult = 0;
+
+const clickCard = function() {
+
+    activeCard = this;
+    if (activeCard == activeCards[0]) return;
+
+    activeCard.classList.remove("hidden");
+
+    if(activeCards.length === 0) {
+        console.log("1")
+        activeCards[0] = activeCard;    
+        return;
+    }
+    
+    else {
+        console.log("2")
+        cards.forEach(card => card.removeEventListener("click", clickCard))
+        activeCards[1] - activeCard;
+        setTimeout(function() { 
+            if (activeCards[0].className === activeCards[1].className) {
+                console.log("wygrane")
+                activeCards.forEach(card => card.classList.add("off")) 
+                gameResult++;
+                cards = cards.filter(card => !card.classList.contains("off"));
+                if(gameResult == gameParis) {
+                    const endTime = new Date().getTime();
+                    const gameTime = (endTime - startTime)/1000
+                    alert(`Udało się! Twój wynik to: ${gameTime} sekund`)
+                    location.reload();
+                }
+            }
+            else {
+                console.log("przegrana")
+                activeCards.forEach(card => card.classList.add("hidden"))  
+            }
+            activeCard = "";
+            activeCards.length = 0
+            cards.forEach(card => card.addEventListener("click", clickCard))
+        }, 500)
+      
+    }
+};
+const init = function() {
+    cards.forEach(card => {
+        const position = Math.floor(Math.random() * cardsColor.length);
         card.classList.add(cardsColor[position]);
-        cardsColor.splice(position, 1)
+        cardsColor.splice(position, 1);
     })
+
+    setTimeout(function() {
+        cards.forEach(card => {
+            card.classList.add("hidden")
+            card.addEventListener("click", clickCard)
+        })
+    }, 2000)
 }
 
 init()
