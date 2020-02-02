a68eba6f71b68bade633b2c3d9ff28356ff6c0ee
Michal Studencki <studi2005@gmail.com>
2020-02-02 16:50:58 +0100

    Add file and finished html and css

---
+++ game.js
@@ -0,0 +1,17 @@
+const cardsColor = ["red", "red", "green", "green", "blue", "blue", "brown", "brown", "yellow", "yellow", "gray", "gray", "cadetblue", "cadetblue", "violet", "violet" , "lightgreen", "lightgreen"];
+
+let cards = document.querySelectorAll("div");
+cards = [...cards];
+
+const init = function() 
+{
+    cards.forEach(function(card)
+    {
+        const position = Math.floor(Math.random()*
+        cardsColor.length);
+        card.classList.add(cardsColor[position]);
+        cardsColor.splice(position, 1)
+    })
+}
+
+init()
